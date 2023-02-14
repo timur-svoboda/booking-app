c196fa687b2a8f465a63f03b2e6ee696661b8de3
Timur Svoboda <svobodatimur@gmail.com>
2023-02-15 01:58:46 +0200

    Add ChakraProvider to web app

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import App from './app/app';
 
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </BrowserRouter>
   </StrictMode>
 );
