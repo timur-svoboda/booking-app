a5a94a74cb512c1ec4e6ea0f6a43d8f3a9ed8fbd
Timur Svoboda <svobodatimur@gmail.com>
2023-02-15 01:35:38 +0200

    Generate web app

---
+++ apps/web/src/app/app.tsx
@@ -0,0 +1,19 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import { Route, Routes, Link } from 'react-router-dom';
+
+export function App() {
+  return (
+    <>
+      <h1>
+        <span> Hello there, </span>
+        Welcome web 👋
+      </h1>
+
+      <div />
+    </>
+  );
+}
+
+export default App;
