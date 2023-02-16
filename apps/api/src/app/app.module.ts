d8dd99ab0ccfff5b2b76a77ba5ea9194817ccfa5
Timur Svoboda <svobodatimur@gmail.com>
2023-02-16 21:47:33 +0200

    Add connection to MongoDB

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { StaysModule } from '@booking-app/api/stays';
 
 @Module({
-  imports: [StaysModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGODB_CONNECTION_URI),
+    StaysModule,
+  ],
   controllers: [],
   providers: [],
 })
