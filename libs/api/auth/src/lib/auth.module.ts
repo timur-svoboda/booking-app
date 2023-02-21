3933b91d0a16bcec39886d767b3edea15d54c26a
Timur Svoboda <svobodatimur@gmail.com>
2023-02-21 15:04:13 +0200

    Improve file structure of api-auth

--- libs/api/auth/src/lib/auth.module.ts
+++ libs/api/auth/src/lib/auth.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtStrategy } from './jwt.strategy';
-import { JwtAuthGuard } from './jwt-auth.guard';
-import { PermissionsGuard } from './permissions.guard';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { PermissionsGuard } from './guards/permissions.guard';
 
 @Module({
   providers: [JwtStrategy, JwtAuthGuard, PermissionsGuard],
