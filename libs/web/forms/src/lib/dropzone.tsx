09eafb6d0f55c5beb7b027b0116b66a51f6c8e4d
Timur Svoboda <svobodatimur@gmail.com>
2023-02-26 23:55:54 +0200

    Move ImageDropzone UI into Dropzone component

---
+++ libs/web/forms/src/lib/dropzone.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { DropzoneOptions, useDropzone } from 'react-dropzone';
+import { useToken, Box, Center, Text, Spinner } from '@chakra-ui/react';
+
+/* eslint-disable-next-line */
+export interface DropzoneProps extends DropzoneOptions {
+  hintText?: string;
+  isLoading?: boolean;
+}
+
+export function Dropzone({
+  hintText,
+  isLoading,
+  ...dropzoneOptions
+}: DropzoneProps) {
+  /* Logic */
+  const { getRootProps, getInputProps, isFocused, isDragAccept, isDragReject } =
+    useDropzone(dropzoneOptions);
+
+  /* Styles */
+  const [red500, blue500] = useToken('colors', ['red.500', 'blue.500']);
+
+  const borderColor = React.useMemo(() => {
+    if (isDragReject) return red500;
+    if (isDragAccept) return blue500;
+    return 'gray.200';
+  }, [isFocused, isDragAccept, isDragReject]);
+
+  const boxShadow = React.useMemo(() => {
+    if (isDragReject) return `0 0 0 1px ${red500}`;
+    if (isDragAccept) return `0 0 0 1px ${blue500}`;
+    return '';
+  }, [isFocused, isDragAccept, isDragReject]);
+
+  /* Markup */
+  return (
+    <Box
+      {...getRootProps()}
+      padding="8px 16px"
+      height="20"
+      borderWidth={1}
+      borderStyle="solid"
+      borderColor={borderColor}
+      borderRadius="md"
+      boxShadow={boxShadow}
+      cursor={isLoading ? 'wait' : 'pointer'}
+      transition="all 0.2s ease"
+    >
+      <input {...getInputProps()} />
+
+      <Center height="100%">
+        {isLoading ? (
+          <Spinner cursor="wait" />
+        ) : (
+          <Text color="gray.500" cursor="pointer">
+            {hintText ??
+              "Drag 'n' drop some files here, or click to select files"}
+          </Text>
+        )}
+      </Center>
+    </Box>
+  );
+}
+
+export default Dropzone;
--- libs/web/forms/src/lib/image-dropzone.tsx
+++ libs/web/forms/src/lib/image-dropzone.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import { useDropzone, DropzoneOptions } from 'react-dropzone';
-import { useToken, Box, Center, Text, Spinner } from '@chakra-ui/react';
+import { DropzoneOptions } from 'react-dropzone';
 import {
   Control,
   FieldPath,
   FieldValues,
   useController,
 } from 'react-hook-form';
+import { Dropzone } from './dropzone';
 
 /* eslint-disable-next-line */
 export interface ImageDropzoneProps<
@@ -26,80 +25,35 @@ export function ImageDropzone<
   TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
   TContext = any
 >(props: ImageDropzoneProps<TFieldValues, TName, TContext>) {
-  /* Logic */
   const { field } = useController({
     name: props.name,
     control: props.control,
     rules: { required: props.required },
   });
 
-  const { getRootProps, getInputProps, isFocused, isDragAccept, isDragReject } =
-    useDropzone({
-      disabled: props.isLoading,
-      accept: {
-        'image/*': [],
-      },
-      onDrop: (acceptedFiles, fileRejections, event) => {
-        if (Array.isArray(field.value)) {
-          field.onChange([...field.value, ...acceptedFiles]);
-        } else {
-          field.onChange(acceptedFiles);
-        }
+  const onDrop: DropzoneOptions['onDrop'] = (
+    acceptedFiles,
+    fileRejections,
+    event
+  ) => {
+    if (Array.isArray(field.value)) {
+      field.onChange([...field.value, ...acceptedFiles]);
+    } else {
+      field.onChange(acceptedFiles);
+    }
 
-        if (props.onDrop) {
-          props.onDrop(acceptedFiles, fileRejections, event);
-        }
-      },
-    });
+    if (props.onDrop) {
+      props.onDrop(acceptedFiles, fileRejections, event);
+    }
+  };
 
-  /* Styles */
-  const [red500, blue500] = useToken('colors', ['red.500', 'blue.500']);
-
-  const borderColor = React.useMemo(() => {
-    if (isDragReject) return red500;
-    if (isDragAccept) return blue500;
-    return 'gray.200';
-  }, [isFocused, isDragAccept, isDragReject]);
-
-  const boxShadow = React.useMemo(() => {
-    if (isDragReject) return `0 0 0 1px ${red500}`;
-    if (isDragAccept) return `0 0 0 1px ${blue500}`;
-    return '';
-  }, [isFocused, isDragAccept, isDragReject]);
-
-  /* Markup */
   return (
-    <Box as="section">
-      <Box
-        {...getRootProps()}
-        padding="8px 16px"
-        height="20"
-        borderWidth={1}
-        borderStyle="solid"
-        borderColor={borderColor}
-        borderRadius="md"
-        boxShadow={boxShadow}
-        cursor={props.isLoading ? 'normal' : 'pointer'}
-        transition="all 0.2s ease"
-      >
-        <input
-          {...getInputProps()}
-          onBlur={field.onBlur}
-          name={field.name}
-          ref={field.ref}
-        />
-
-        <Center height="100%">
-          {props.isLoading ? (
-            <Spinner />
-          ) : (
-            <Text color="gray.500" cursor="pointer">
-              Drag 'n' drop some files here, or click to select files
-            </Text>
-          )}
-        </Center>
-      </Box>
-    </Box>
+    <Dropzone
+      accept={{ 'image/*': [] }}
+      isLoading={props.isLoading}
+      disabled={props.isLoading}
+      onDrop={onDrop}
+    />
   );
 }
 
