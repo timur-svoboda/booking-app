6bba75a9e80c6d101997322b9134815337d8fabd
Timur Svoboda <svobodatimur@gmail.com>
2023-03-06 14:17:09 +0200

    Add prod environment for web

--- apps/web/src/environments/environment.prod.ts
+++ apps/web/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
   production: true,
+  auth0: {
+    domain: 'booking-app-dev.eu.auth0.com',
+    clientId: 'EOz9IkVaOik8Dy1x2AIrSKouWYwtitYy',
+    audience: 'https://booking-app.api',
+  },
 };
