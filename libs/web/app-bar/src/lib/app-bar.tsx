9faf3ca9166cdc1ba63a10c227130d95d8bfdc31
Timur <svobodatimur@gmail.com>
2023-03-06 14:04:01 +0200

    Add feature for users to navigate (#11)

--- libs/web/app-bar/src/lib/app-bar.tsx
+++ libs/web/app-bar/src/lib/app-bar.tsx
@@ -1,43 +1,61 @@
-import { Container, Flex } from '@chakra-ui/layout';
-import { Link } from 'react-router-dom';
-import { Box, Button } from '@chakra-ui/react';
+import { Center, Container, Flex } from '@chakra-ui/layout';
+import { Box, Button, Slide } from '@chakra-ui/react';
+import { HamburgerIcon } from '@chakra-ui/icons';
 import { AuthButtons } from '@booking-app/web/auth';
-import { useAuth0 } from '@auth0/auth0-react';
-import { AddStayPageLink, OwnStaysListPageLink } from '@booking-app/web/stays';
+import Navigation from './navigation';
+import React from 'react';
 
 /* eslint-disable-next-line */
 export interface AppBarProps {}
 
 export function AppBar(props: AppBarProps) {
-  const { isAuthenticated } = useAuth0();
+  const [isMobileMenuVisible, setIsMobileMenuVisible] = React.useState(false);
+
+  React.useEffect(() => {
+    if (isMobileMenuVisible) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+
+    () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [isMobileMenuVisible]);
 
   return (
-    <Box borderBottomWidth="1px" borderColor="gray.200">
+    <Box padding="12px 0" borderBottomWidth="1px" borderColor="gray.200">
       <Container maxW="100%">
-        <Flex justifyContent="flex-end" pt={2} pb={2}>
-          <Box pr={2}>{isAuthenticated && <AddStayPageLink />}</Box>
-          <Box pr={2}>
-            {isAuthenticated && (
-              <Link to="/reservations/my">
-                <Button as="span" colorScheme="teal" variant="ghost">
-                  My Reservations
-                </Button>
-              </Link>
-            )}
+        <Flex justify="space-between" align="center">
+          <Box display={{ base: 'none', lg: 'block' }}>
+            <Navigation />
           </Box>
-          <Box pr={2}>
-            {isAuthenticated && (
-              <Link to="/reservations/my-stays">
-                <Button as="span" colorScheme="teal" variant="ghost">
-                  My Stays Reservations
-                </Button>
-              </Link>
-            )}
-          </Box>
-          <Box pr={2}>{isAuthenticated && <OwnStaysListPageLink />}</Box>
+          <Button
+            w="48px"
+            h="48px"
+            display={{ lg: 'none' }}
+            colorScheme="teal"
+            variant="ghost"
+            onClick={() => setIsMobileMenuVisible(!isMobileMenuVisible)}
+          >
+            <Center as="span">
+              <HamburgerIcon w="32px" h="32px" />
+            </Center>
+          </Button>
           <AuthButtons />
         </Flex>
       </Container>
+      <Box display={{ lg: 'none' }}>
+        <Slide
+          direction="left"
+          in={isMobileMenuVisible}
+          style={{ position: 'absolute', top: 72, zIndex: 10, height: '100vh' }}
+        >
+          <Box w="100%" h="100%" p="24px 16px" bg="white">
+            <Navigation onNavigate={() => setIsMobileMenuVisible(false)} />
+          </Box>
+        </Slide>
+      </Box>
     </Box>
   );
 }
---
+++ libs/web/app-bar/src/lib/navigation.tsx
@@ -0,0 +1,70 @@
+import { Button, Stack } from '@chakra-ui/react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Link } from 'react-router-dom';
+
+/* eslint-disable-next-line */
+export interface NavigationProps {
+  onNavigate?: () => void;
+}
+
+export interface NavigationLink {
+  title: string;
+  url: string;
+  protected?: boolean;
+}
+
+export const navigationLinks: NavigationLink[] = [
+  {
+    title: 'Home',
+    url: '/',
+  },
+  {
+    title: 'Add Stay',
+    url: '/stays/add-new',
+    protected: true,
+  },
+  {
+    title: 'My Stays',
+    url: '/stays/my',
+    protected: true,
+  },
+  {
+    title: 'My Reservations',
+    url: '/reservations/my',
+    protected: true,
+  },
+  {
+    title: 'My Stays Reservations',
+    url: '/reservations/my-stays',
+    protected: true,
+  },
+];
+
+export function Navigation(props: NavigationProps) {
+  const { isAuthenticated } = useAuth0();
+
+  const allowedNavigationLinks = navigationLinks.filter((navigationLink) => {
+    return (
+      !navigationLink.protected || (navigationLink.protected && isAuthenticated)
+    );
+  });
+
+  return (
+    <Stack direction={{ base: 'column', lg: 'row' }} spacing="8px">
+      {allowedNavigationLinks.map((navigationLink) => (
+        <Link to={navigationLink.url}>
+          <Button
+            as="span"
+            colorScheme="teal"
+            variant="ghost"
+            onClick={props.onNavigate}
+          >
+            {navigationLink.title}
+          </Button>
+        </Link>
+      ))}
+    </Stack>
+  );
+}
+
+export default Navigation;
